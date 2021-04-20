f4e213546042d04a61714037de2cfea5affc2c03
nohustoe <82841335+nohustoe@users.noreply.github.com>
2021-04-20 23:18:33 +0200

    Delete trees_01.tsx

--- trees_01.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="trees_01" tilewidth="32" tileheight="32" tilecount="40320" columns="120">
- <image source="../adventure_world-Nora/trees_01.png" width="3840" height="10752"/>
-</tileset>
